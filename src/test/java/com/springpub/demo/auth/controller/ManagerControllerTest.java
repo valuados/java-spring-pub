9116a9dd59ba7b2b66c234716a0a1e963c4edb73
valuados <valuados@gmail.com>
2020-02-17 14:18:28 +0300

    removing outdated methods from manager controller and adding pom xml update

--- src/test/java/com/springpub/demo/auth/controller/ManagerControllerTest.java
+++ src/test/java/com/springpub/demo/auth/controller/ManagerControllerTest.java
@@ -22,42 +22,4 @@ public class ManagerControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    public void testManagerCreateMenuItem() throws Exception {
-        mockMvc.perform(post("/manager/menu/add")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\n" +
-                        "    \"title\" : \"Zubrowka\",\n" +
-                        "    \"portion\" : 50, \n" +
-                        "    \"bottleVolume\" : 1000,\n" +
-                        "    \"portionPrice\" : 5, \n" +
-                        "    \"bottlePrice\" : 50, \n" +
-                        "    \"strength\" : 40,\n" +
-                        "    \"description\" : \"Водка Зубровка\"\n" +
-                        "}"))
-                // then
-                .andExpect(status().isCreated())
-                .andExpect(content().json("{\n" +
-                        "  \"id\" : 1\n" +
-                        "}"));
-    }
-
-    @Test
-    public void testManagerDeleteMenuItem() throws Exception {
-        mockMvc.perform(delete("manager/menu/delete/{itemId}"))
-                // then
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void testManagerUpdatePrice() throws Exception {
-        mockMvc.perform(put("manager/menu/update")
-                .header("menuItemId", (int)1)
-                .header("price", (double)8.30))
-                // then
-                .andExpect(status().isOk())
-                .andExpect(content().json("{\n" +
-                        "    \"id\" : 1\n" +
-                        "}"));
-    }
 }
